2ba389665031e862a787f3879dea453fdf1ec677
Chris Joyce <chris@Chriss-iMac.modem>
2021-02-08 18:52:45 +1000

    Add Results component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <h1>vue-reaction-timer</h1>
+  <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <p v-if="showResults">Reaction time: {{ score }} ms</p>
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
+// when the game ends, show the results component
+// output the score inside the results component
 import Block from "./components/Block";
-
+import Results from "./components/Results";
 export default {
   name: "App",
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
@@ -21,11 +23,11 @@ export default {
   },
   methods: {
     start() {
+      // set time amount (ms)
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
       this.showResults = false;
     },
-
     endGame(reactionTime) {
       this.score = reactionTime;
       this.isPlaying = false;
@@ -43,5 +45,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
